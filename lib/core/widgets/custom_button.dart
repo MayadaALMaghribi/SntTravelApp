cd36289933f3453feeca92ae5209b28655bf7e08
dinamohmed311 <153089812+dinamohmed311@users.noreply.github.com>
2025-02-17 11:52:54 +0200

    custom button done

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -24,55 +24,8 @@ class CustomButton extends StatelessWidget {
           minimumSize: const Size(double.infinity, 48),
         ),
         onPressed: ontap,
-        child: child,
+        child: child, 
       ),
     );
   }
 }
-
-
-//  Padding(
-//                       padding: const EdgeInsets.all(16),
-//                       child: ElevatedButton(
-//                         onPressed: state is SignInLoading
-//                             ? null // تعطيل الزر أثناء التحميل
-//                             : () {
-//                                 if (context
-//                                         .read<UserCubit>()
-//                                         .signInEmail
-//                                         .text
-//                                         .isNotEmpty &&
-//                                     context
-//                                         .read<UserCubit>()
-//                                         .signInPassword
-//                                         .text
-//                                         .isNotEmpty) {
-//                                   context.read<UserCubit>().signIn();
-//                                 } else {
-//                                   ScaffoldMessenger.of(context).showSnackBar(
-//                                     const SnackBar(
-//                                         content:
-//                                             Text('Please fill all fields')),
-//                                   );
-//                                 }
-//                               },
-//                         style: ElevatedButton.styleFrom(
-//                           padding: const EdgeInsets.symmetric(vertical: 16),
-//                           minimumSize: const Size(double.infinity, 48),
-//                           backgroundColor: const Color(0xff49AEE4),
-//                         ),
-//                         child: state is SignInLoading
-//                             ? const SizedBox(
-//                                 height: 24,
-//                                 width: 24,
-//                                 child: CircularProgressIndicator(
-//                                   color: Colors.white,
-//                                   strokeWidth: 2.5,
-//                                 ),
-//                               )
-//                             : const Text(
-//                                 'Login',
-//                                 style: TextStyle(color: Colors.white),
-//                               ),
-//                       ),
-//                     ),
