713973542a06ece2a30a10169019b18f45af53e0
MayadaALMaghribi <latifa293h@gmail.com>
2025-05-16 12:58:21 -0700

    more modification on the payment api

--- lib/Features/payment/presentation/views/payment_view.dart
+++ lib/Features/payment/presentation/views/payment_view.dart
@@ -1,11 +1,18 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sntegpito/Features/payment/presentation/manager/cancel_booking/cancel_booking_cubit.dart';
 import 'package:sntegpito/Features/payment/presentation/views/widgets/payment_view_body.dart';
+import 'package:sntegpito/core/api/dio_consumer.dart';
 
 class PaymentView extends StatelessWidget {
   const PaymentView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const PaymentViewBody();
+    return BlocProvider(
+      create: (context) => CancelBookingCubit(DioConsumer(dio: Dio())),
+      child: const PaymentViewBody(),
+    );
   }
 }
