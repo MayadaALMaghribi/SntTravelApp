cf0c3810e47f91c5d1e43fb3e8c118d7dbfd9e7e
MayadaALMaghribi <latifa293h@gmail.com>
2025-06-01 23:27:26 -0700

    g

--- lib/core/utils/open_app_orstore.dart
+++ lib/core/utils/open_app_orstore.dart
@@ -12,19 +12,3 @@ Future<void> openOrInstallApp({
     await launchUrl(Uri.parse(playStoreUrl));
   }
 }
-
-
-/**
- * import 'package:url_launcher/url_launcher.dart';
-
-Future<void> openAppOrStore({required String packageName}) async {
-  final Uri intentUri = Uri.parse("intent://#Intent;package=$packageName;end");
-  final Uri playStoreUri =
-      Uri.parse("https://play.google.com/store/apps/details?id=$packageName");
-  if (await canLaunchUrl(intentUri)) {
-    await launchUrl(intentUri, mode: LaunchMode.externalApplication);
-  } else {
-    await launchUrl(playStoreUri, mode: LaunchMode.externalApplication);
-  }
-}
- */
